e9732f99cd9a82017efbb048a928cd122f279698
sameer-laxyo <sameerborde.sam@gmail.com>
2020-07-11 18:21:52 +0530

    (2)Live head tag remove after push 2020-07-11

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 
-// Route::get('login/{username}/{pass}', 'LoginController@login');
-// Route::post('/logout', 'LoginController@logout')->name('logout');
-Auth::routes(['register' => false]);
+ Route::get('login/{username}/{pass}', 'LoginController@login');
+ Route::post('/logout', 'LoginController@logout')->name('logout');
+//Auth::routes(['register' => false]);
 
 
 Route::resource('/information', 'InformationController');
